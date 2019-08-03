01ebbc5f12e72aa633a9770f7d0bba171ab0e15f
fabiodmioto <fabiodmioto@gmail.com>
2019-08-03 12:49:56 -0300

    Instanciando dados no banco ao startar o projeto

--- src/main/java/br/com/fabiodmioto/cursomc/CursomcApplication.java
+++ src/main/java/br/com/fabiodmioto/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package br.com.fabiodmioto.cursomc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.fabiodmioto.cursomc.Services.CategoriaService;
+import br.com.fabiodmioto.cursomc.domain.Categoria;
+import br.com.fabiodmioto.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository cr;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(new Categoria(null, "Informática"));
+		categorias.add(new Categoria(null, "Escritório"));
+		
+		cr.saveAll(categorias);
+		
 	}
 
 }
